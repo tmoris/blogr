1800801c07f0ebb787e1690940a4182dae21cc87
Tibenkana Moris <moristotau@gmail.com>
2023-09-21 15:37:37 +0400

    Updated blog/models.py

--- blog/blog.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from config import Config
-
-
-def create_app(config_class=Config):
-    app = Flask(__name__)
-    app.config.from_object(Config)
-    return app
--- blog/models.py
+++ blog/models.py
@@ -1,28 +1,30 @@
-from flask_login import UserMixins
+from flask_login import UserMixin
 from flask import current_app
-from datatime import datetime
-from itsdangerous.url_safe import 
-URLSafeTimedSerializer as Serializer
+from datetime import datetime
+
 from flask_sqlalchemy import SQLAlchemy
+
 db = SQLAlchemy()
 
-class User(db.Model, UserM:ixin):
-	id = db.Column(db.Intenger, primary_key=True)
-	username = db.Column(db.String(20), unique=True, nullable=False)
-	email = db.Column(db.String(255), unique=True, nullable=False)
-	image_file = db.Column(db.String(60), nullable=False, default="default.jpg")
-	password = db.Column(String(60), nullable=False)
-	post = db.relationship("Post", backref="author", lazy=True)
 
-	def __repr__(self):
-		return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    image_file = db.Column(db.String(60), nullable=False, default="default.jpg")
+    password = db.Column(db.String(60), nullable=False)
+    post = db.relationship("Post", backref="author", lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
 
 class Post(db.Model):
-	id = db.Column(Intenger, primary_key=True),
-	title = db.Column(String(100), nullable=False)
-	date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-	content = db.Column(db.Text, nullable=False)
-	user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
 
-	def __repr__(self):
-		return f"Post('{self.title}', '{self.content}', '{self.date_posted}')"
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.content}', '{self.date_posted}')"
